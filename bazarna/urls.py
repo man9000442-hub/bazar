2c96ad6863cbd9e8ff11ba2940ef1d72df221a78
unknown <man9000442@gmail.com>
2026-02-18 06:18:07 +0200

    fix

--- bazarna/urls.py
+++ bazarna/urls.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import signup_choice
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/signup/', signup_choice, name='account_signup'),
-    path('accounts/', include('allauth.urls')), 
-    path('user/', include('accounts.urls')), # مساراتنا الخاصة (مثل إكمال البروفايل)
-    path('', include('store.urls')),
-    path('merchant/', include('merchant_panel.urls')),
-    path('super/', include('supervisor.urls')), 
-]
+from django.views.static import serve
+from accounts.views import signup_choice # تأكد أن هذا الـ View موجود
 
-# لكي تظهر الصور أثناء التطوير
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    # لوحة الأدمن
+    path('admin/', admin.site.urls),
+    
+    # حسابات المستخدمين
+    path('accounts/signup/', signup_choice, name='account_signup'), # خطف رابط التسجيل
+    path('accounts/', include('allauth.urls')),
+    path('user/', include('accounts.urls')),
+    
+    # تطبيقاتنا
+    path('merchant/', include('merchant_panel.urls')),
+    path('super/', include('supervisor.urls')),
+    path('', include('store.urls')),
+    
+    # --- الروابط السحرية لتشغيل الصور (حتى لو DEBUG=False) ---
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
