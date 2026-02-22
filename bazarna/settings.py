34860873e5543778b2579a5101c60cf0d0e62fc0
unknown <man9000442@gmail.com>
2026-02-23 00:37:47 +0200

    fix

--- bazarna/settings.py
+++ bazarna/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url # تحتاج pip install dj-database-url
+from dotenv import load_dotenv
 from pathlib import Path
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
